7d1cc7f7a2dea9057b079617a96ca3a0a644a9c6
Thomas Weber <thomas.weber@elektrotauchner.at>
2020-01-15 16:47:57 +0100

    added license to source files

--- tools/nand/nandsplit.py
+++ tools/nand/nandsplit.py
@@ -1,3 +1,13 @@
+# 
+# Copyright (C): SEC Consult Unternehmensberatung GmbH, 2019         \n
+# Web:           https://sec-consult.com/                            \n
+#                                                                    \n
+# This Source Code Form is subject to the terms of the Mozilla Public\n
+# License, v. 2.0. If a copy of the MPL was not distributed with this\n
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.          \n
+# 
+# @authors Wolfgang Ettlinger
+#
 import sys
 
 
